1f78ee688f38f770e7e9d38ab95cc94893549bc6
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-06 18:14:36 +0200

    dev changed test_services and test_validators logic after code refactor

--- tests/forager_service/test_services.py
+++ tests/forager_service/test_services.py
@@ -3,7 +3,7 @@ import pytest
 from faker import Faker
 
 from forager_service.exceptions import ArgumentValidationError
-from forager_service.utils import create_and_validate_params, validate_storage_key
+from forager_service.services import create_and_validate_params
 
 
 class TestCreateAdnValidateParams:
@@ -11,26 +11,24 @@ class TestCreateAdnValidateParams:
 
     def test_create_and_validate_params(self, faker: Faker, get_kwargs: dict) -> None:
         """Test create_and_validate_params."""
-        kwargs: dict = get_kwargs
+        kwargs_dict: dict = get_kwargs
         operation_type: str = faker.random_element(
-            elements=("email-count", "email_finder", "domain_search")
+            elements=("email-count", "email-verifier", "email-finder", "domain-search")
         )
+        kwargs: dict = kwargs_dict.get(operation_type)
         result = create_and_validate_params(operation_type, **kwargs)
         for key, value in kwargs.items():
             assert result[key] == value
 
-    def test_create_and_validate_params_some_empty(
-        self, faker: Faker, get_kwargs: dict
-    ) -> None:
+    def test_create_and_validate_params_some_empty(self, faker: Faker, get_kwargs: dict) -> None:
         """Test create_and_validate_params."""
-        kwargs: dict = get_kwargs
+        kwargs_dict: dict = get_kwargs
+        operation_type: str = faker.random_element(elements=("email-count", "email-finder", "domain-search"))
+        kwargs: dict = kwargs_dict.get(operation_type)
         number = faker.random_int(max=len(kwargs) - 1)
         for i, key in enumerate(kwargs):
-            if i == number or i == number - 2:
+            if i == number and key not in {"domain", "company"}:
                 kwargs[key] = None
-        operation_type: str = faker.random_element(
-            elements=("email-count", "email_finder", "domain_search")
-        )
         result = create_and_validate_params(operation_type, **kwargs)
         for key, value in kwargs.items():
             if value is None:
@@ -38,18 +36,17 @@ class TestCreateAdnValidateParams:
             else:
                 assert result[key] == value
 
-    def test_create_and_validate_params_error(
-        self, faker: Faker, get_kwargs: dict
-    ) -> None:
+    def test_create_and_validate_params_error(self, faker: Faker, get_kwargs: dict) -> None:
         """Test create_and_validate_params."""
-        kwargs: dict = get_kwargs
+        kwargs_dict: dict = get_kwargs
+        operation_type: str = faker.random_element(
+            elements=("email-count", "email-verifier", "email-finder", "domain-search")
+        )
+        kwargs: dict = kwargs_dict.get(operation_type)
         number = faker.random_int(max=len(kwargs) - 1)
         for i, key in enumerate(kwargs):
             if i == number:
                 kwargs[key] = set()
-        operation_type: str = faker.random_element(
-            elements=("email-count", "email_finder", "domain_search")
-        )
         with pytest.raises(ArgumentValidationError):
             create_and_validate_params(operation_type, **kwargs)
 
@@ -57,44 +54,24 @@ class TestCreateAdnValidateParams:
         self, faker: Faker, get_kwargs: dict
     ) -> None:
         """Test create_and_validate_params."""
-        kwargs: dict = get_kwargs
-        elements = faker.random_element(
-            elements=[("full_name", "first_name"), ("full_name", "last_name")]
-        )
+        kwargs_dict: dict = get_kwargs
+        operation_type: str = "email-finder"
+        kwargs: dict = kwargs_dict.get(operation_type)
+        elements = faker.random_element(elements=[("full_name", "first_name"), ("full_name", "last_name")])
         for elem in elements:
             if kwargs.get(elem):
                 kwargs[elem] = None
-        operation_type: str = "email-finder"
         with pytest.raises(ArgumentValidationError):
             create_and_validate_params(operation_type, **kwargs)
 
-    def test_create_and_validate_params_error_required_arguments(
-        self, faker: Faker, get_kwargs: dict
-    ) -> None:
+    def test_create_and_validate_params_error_required_arguments(self, faker: Faker, get_kwargs: dict) -> None:
         """Test create_and_validate_params."""
-        kwargs: dict = get_kwargs
+        kwargs_dict: dict = get_kwargs
+        operation_type: str = faker.random_element(elements=("email-count", "email-finder", "domain-search"))
+        kwargs: dict = kwargs_dict.get(operation_type)
         elements = ("company", "domain")
         for elem in elements:
             if kwargs.get(elem) is not None:
                 kwargs[elem] = None
-        operation_type: str = faker.random_element(
-            elements=("email-count", "email-finder", "domain-search")
-        )
         with pytest.raises(ArgumentValidationError):
             create_and_validate_params(operation_type, **kwargs)
-
-
-class TestValidateStorageKey:
-    """Class for testing validate_storage_key method."""
-
-    def test_validate_storage_key(self, faker: Faker) -> None:
-        """Test validate_storage_key."""
-        argument: str = faker.pystr(min_chars=3, max_chars=10)
-        validate_storage_key(argument)
-
-    def test_validate_storage_key_error(self, faker: Faker) -> None:
-        """Test validate_storage_key, error case."""
-        argument: int = faker.random_int()
-        with pytest.raises(ArgumentValidationError) as ex_info:
-            validate_storage_key(argument)
-        assert str(ex_info.value) == "argument has wrong type."
--- tests/forager_service/test_validators.py
+++ tests/forager_service/test_validators.py
@@ -3,65 +3,69 @@ import pytest
 from faker import Faker
 
 from forager_service.exceptions import ArgumentValidationError
-from forager_service.validators import Validators
+from forager_service.validators import (
+    common_validators,
+    kwargs_validators,
+    special_validators,
+)
 
 
 class TestValidatorsValidateStr:
-    """Class for testing Validators validate_str method."""
+    """Class for testing common_validators validate_str method."""
 
     def test_validate_str(self, faker: Faker) -> None:
         """Test validate_str."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
         value: str = faker.pystr(min_chars=3, max_chars=7)
-        Validators.validate_str(key, value)
+        common_validators.validate_str(key, value)
 
     def test_validate_str_error(self, faker: Faker) -> None:
         """Test validate_str, error case."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
         value: int = faker.random_int()
         with pytest.raises(ArgumentValidationError) as ex_info:
-            Validators.validate_str(key, value)
+            common_validators.validate_str(key, value)
         assert str(ex_info.value) == f"{key} has wrong type."
 
 
 class TestValidatorsValidateInt:
-    """Class for testing Validators validate_int method."""
+    """Class for testing common_validators validate_int method."""
 
     def test_validate_int(self, faker: Faker) -> None:
         """Test validate_int."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
         value: int = faker.random_int()
-        Validators.validate_int(key, value)
+        common_validators.validate_int(key, value)
 
     def test_validate_int_error(self, faker: Faker) -> None:
         """Test validate_int, error case."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
         value: str = faker.city()
         with pytest.raises(ArgumentValidationError) as ex_info:
-            Validators.validate_int(key, value)
+            common_validators.validate_int(key, value)
         assert str(ex_info.value) == f"{key} has wrong type."
 
 
 class TestValidatorsValidateEmailType:
-    """Class for testing Validators validate_email_type method."""
+    """Class for testing kwargs_validators validate_email_type method."""
 
     def test_validate_email_type(self, faker: Faker) -> None:
         """Test validate_email_type."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
         value: str = faker.random_element(elements=("personal", "generic"))
-        Validators.validate_email_type(key, value)
+        kwargs_validators.validate_email_type(key, value)
 
     def test_validate_email_type_error(self, faker: Faker) -> None:
         """Test validate_email_type, error case."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
         value: str = faker.city()
         with pytest.raises(ArgumentValidationError) as ex_info:
-            Validators.validate_email_type(key, value)
+            kwargs_validators.validate_email_type(key, value)
         assert str(ex_info.value) == f"{key} has wrong value."
 
 
 class TestValidatorsValidateSeniority:
-    """Class for testing Validators validate_seniority method."""
+    """Class for testing kwargs_validators validate_seniority method."""
 
     def test_validate_seniority(self, faker: Faker) -> None:
         """Test validate_seniority."""
@@ -75,19 +79,19 @@ class TestValidatorsValidateSeniority:
                 "senior, junior, executive",
             )
         )
-        Validators.validate_seniority(key, value)
+        kwargs_validators.validate_seniority(key, value)
 
     def test_validate_seniority_error(self, faker: Faker) -> None:
         """Test validate_seniority, error case."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
         value: str = faker.city()
         with pytest.raises(ArgumentValidationError) as ex_info:
-            Validators.validate_seniority(key, value)
+            kwargs_validators.validate_seniority(key, value)
         assert str(ex_info.value) == f"{key} has wrong value."
 
 
 class TestValidatorsValidateDepartment:
-    """Class for testing Validators validate_department method."""
+    """Class for testing kwargs_validators validate_department method."""
 
     def test_validate_department(self, faker: Faker) -> None:
         """Test validate_department."""
@@ -108,19 +112,19 @@ class TestValidatorsValidateDepartment:
                 )
             )
         )
-        Validators.validate_department(key, value)
+        kwargs_validators.validate_department(key, value)
 
     def test_validate_department_error(self, faker: Faker) -> None:
         """Test validate_department, error case."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
         value: str = faker.city()
         with pytest.raises(ArgumentValidationError) as ex_info:
-            Validators.validate_department(key, value)
+            kwargs_validators.validate_department(key, value)
         assert str(ex_info.value) == f"{key} has wrong value."
 
 
 class TestValidatorsValidateRequiredField:
-    """Class for testing Validators validate_required_field method."""
+    """Class for testing kwargs_validators validate_required_field method."""
 
     def test_validate_required_field(self, faker: Faker) -> None:
         """Test validate_required_field."""
@@ -134,97 +138,94 @@ class TestValidatorsValidateRequiredField:
                 )
             )
         )
-        Validators.validate_required_field(key, value)
+        kwargs_validators.validate_required_field(key, value)
 
     def test_validate_required_field_error(self, faker: Faker) -> None:
         """Test validate_required_field, error case."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
         value: str = faker.city()
         with pytest.raises(ArgumentValidationError) as ex_info:
-            Validators.validate_required_field(key, value)
+            kwargs_validators.validate_required_field(key, value)
         assert str(ex_info.value) == f"{key} has wrong value."
 
 
 class TestValidatorsValidateMaxDuration:
-    """Class for testing Validators validate_max_duration method."""
+    """Class for testing kwargs_validators validate_max_duration method."""
 
     def test_validate_max_duration(self, faker: Faker) -> None:
         """Test validate_max_duration."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
         value: int = faker.random_int(min=3, max=20)
-        Validators.validate_max_duration(key, value)
+        kwargs_validators.validate_max_duration(key, value)
 
     def test_validate_max_duration_error(self, faker: Faker) -> None:
         """Test validate_max_duration, error case."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
-        value: int = faker.random_int(min=21)
+        value: int = faker.random_int(min=21, max=1000)
         with pytest.raises(ArgumentValidationError) as ex_info:
-            Validators.validate_max_duration(key, value)
+            kwargs_validators.validate_max_duration(key, value)
         assert str(ex_info.value) == f"{key} should be in range from 3 to 20."
 
 
 class TestValidatorsValidateEmail:
-    """Class for testing Validators validate_email method."""
+    """Class for testing common_validators validate_email method."""
 
     def test_validate_email(self, faker: Faker) -> None:
         """Test validate_email."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
         value: str = faker.email()
-        Validators.validate_email(key, value)
+        common_validators.validate_email(key, value)
 
     def test_validate_email_error(self, faker: Faker) -> None:
         """Test validate_email, error case."""
         key: str = faker.pystr(min_chars=3, max_chars=7)
         value: str = faker.pystr(min_chars=3)
         with pytest.raises(ArgumentValidationError) as ex_info:
-            Validators.validate_email(key, value)
+            common_validators.validate_email(key, value)
         assert str(ex_info.value) == f"{key} has invalid value."
 
 
 class TestValidatorsValidateRequiredArguments:
-    """Class for testing Validators validate_required_arguments method."""
+    """Class for testing special_validators validate_required_arguments method."""
 
     def test_validate_required_arguments(self, faker: Faker) -> None:
         """Test validate_required_arguments."""
         params: dict = dict()
-        key: str = faker.random_element(
-            elements=("domain-search", "email-finder", "email-count")
-        )
+        key: str = faker.random_element(elements=("domain-search", "email-finder", "email-count"))
         for elem in faker.random_choices(elements=("domain", "company")):
             params[elem] = faker.city()
         if key == "email-finder":
-            elements = faker.random_element(
-                elements=[("full_name",), ("first_name", "last_name")]
-            )
+            elements = faker.random_element(elements=[("full_name",), ("first_name", "last_name")])
             for elem in elements:
                 params[elem] = faker.city()
-        Validators.validate_required_arguments(key, params)
+        special_validators.validate_required_arguments(key, params)
 
     def test_validate_required_arguments_error(self, faker: Faker) -> None:
         """Test validate_required_arguments, error case."""
         params: dict = dict()
-        key: str = faker.random_element(
-            elements=("domain-search", "email-finder", "email-count")
-        )
+        key: str = faker.random_element(elements=("domain-search", "email-finder", "email-count"))
         with pytest.raises(ArgumentValidationError) as ex_info:
-            Validators.validate_required_arguments(key, params)
-        assert (
-            str(ex_info.value)
-            == f"For {key} operation should be defined domain or company"
-        )
+            special_validators.validate_required_arguments(key, params)
+        assert str(ex_info.value) == f"For {key} operation should be defined domain or company"
 
-    def test_validate_required_arguments_error_email_finder(self, faker: Faker) -> None:
-        """Test validate_required_arguments."""
+
+class TestValidatorsValidateEmailFinderRequiredArgument:
+    """Class for testing special_validators validate_email_finder_required_argument method."""
+
+    def test_validate_email_finder_required_argument(self, faker: Faker) -> None:
+        """Test validate_email_finder_required_argument."""
         params: dict = dict()
         key: str = "email-finder"
-        for elem in faker.random_choices(elements=("domain", "company")):
+        elements = faker.random_element(elements=[("full_name",), ("first_name", "last_name")])
+        for elem in elements:
             params[elem] = faker.city()
-        params[
-            faker.random_element(elements=("first_name", "last_name", "11"))
-        ] = faker.city()
+        special_validators.validate_email_finder_required_argument(key, params)
+
+    def test_validate_required_arguments_error_email_finder(self, faker: Faker) -> None:
+        """Test validate_email_finder_required_argument."""
+        params: dict = dict()
+        key: str = "email-finder"
+        params[faker.random_element(elements=("first_name", "last_name", "11"))] = faker.city()
         with pytest.raises(ArgumentValidationError) as ex_info:
-            Validators.validate_required_arguments(key, params)
-        assert (
-            str(ex_info.value) == "At least first_name and last_name or full_name "
-            "should be in params."
-        )
+            special_validators.validate_email_finder_required_argument(key, params)
+        assert str(ex_info.value) == "At least first_name and last_name or full_name " "should be in param_dict."
