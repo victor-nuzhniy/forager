1596bbaceb7a58658f6ad9c6aa8d403e1d9f1a50
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-06 17:16:08 +0200

    dev fixed flake8 issues on validators (with refactor), services, config

--- forager_service/services.py
+++ forager_service/services.py
@@ -1,7 +1,7 @@
 """Services for Forager project."""
 from __future__ import annotations
 
-from forager_service.validators import VALIDATORS, validate_arguments
+from forager_service.validators import special_validators, validators
 
 
 def create_and_validate_params(operation_type: str, **kwargs) -> dict:
@@ -12,13 +12,13 @@ def create_and_validate_params(operation_type: str, **kwargs) -> dict:
     :param kwargs: dict Key word arguments for particular operation.
     :return: dict Params for request.
     """
-    validate_arguments(operation_type, kwargs)
+    special_validators.validate_arguments(operation_type, kwargs)
     param_dict: dict = {}
     for key, element in kwargs.items():
         if element is not None:
-            for validator in VALIDATORS.get(key):
+            for validator in validators.get(key):
                 validator(key, element)
             param_dict[key] = element
-    for validation_handler in VALIDATORS.get("required_arguments"):
+    for validation_handler in validators.get("required_arguments"):
         validation_handler(operation_type, param_dict)
     return param_dict
